4a89190877c250080be421ab5e52d6b6ec80e899
Jens Rud Nielsen <jenrud@gmail.com>
2020-04-12 09:38:00 +0200

    Adds mutex logic.

--- BaseAsyncRunner/Program.cs
+++ BaseAsyncRunner/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using BaseAsyncRunner.Domain;
@@ -32,9 +33,6 @@ namespace BaseAsyncRunner
             var result = Parser.Default.ParseArguments<HeadOptions, TailOptions>(args)
                 .MapResult(app.Run, HandleParseError).GetAwaiter().GetResult();
 
-            //var result = Parser.Default.ParseArguments<HeadOptions>(args)
-            //    .MapResult(async o => await app.Run(o), HandleParseError).Result;
-
             Console.WriteLine("ExitCode=" + result);
             return Task.FromResult(result);
         }
@@ -55,6 +53,10 @@ namespace BaseAsyncRunner
         private readonly IConfig config;
         private readonly IMapper mapper;
 
+        // Instantiates a Singleton of the Semaphore with a value of 1.
+        // This means that only one thread can be granted access at a time.
+        private static readonly SemaphoreSlim mutex = new SemaphoreSlim(1,1);
+
         public ApplicationLogic(ILog log, IConfig config, IMapper mapper)
         {
             this.log = log;
@@ -64,6 +66,11 @@ namespace BaseAsyncRunner
 
         public async Task<int> Run(object options)
         {
+            // Async await to enter the semaphore. If no-one has been granted access to the semaphore,
+            // code execution will proceed, otherwise this thread will wait here until the semaphore
+            // is released.
+            await mutex.WaitAsync();
+
             int result = 0;
             this.log.Write("Quick Start Example!");
             var builder = new StringBuilder("Reading ");
@@ -75,17 +82,19 @@ namespace BaseAsyncRunner
                 {
                     case HeadOptions head:
 
-                        builder.Append(head.Lines ?? head.Bytes).AppendFormat("{0} from top", head.Lines.HasValue ? "lines" : "bytes");
+                        builder.Append(head.Lines ?? head.Bytes)
+                            .AppendFormat("{0} from top", head.Lines.HasValue ? " lines" : "bytes");
                         builder.Append(Environment.NewLine);
                         builder.Append(head.Lines.HasValue
-                            ? ReadLines(head.FileName, true, (int)head.Lines)
-                            : ReadBytes(head.FileName, true, (int)head.Bytes));
+                            ? ReadLines(head.FileName, true, (int) head.Lines)
+                            : ReadBytes(head.FileName, true, (int) head.Bytes));
 
                         break;
 
                     case TailOptions tail:
 
-                        builder.Append(tail.Lines ?? tail.Bytes).AppendFormat("{0} from top", tail.Lines.HasValue ? "lines" : "bytes");
+                        builder.Append(tail.Lines ?? tail.Bytes)
+                            .AppendFormat("{0} from top", tail.Lines.HasValue ? " lines" : "bytes");
                         builder.Append(Environment.NewLine);
                         builder.Append(tail.Lines.HasValue
                             ? ReadLines(tail.FileName, false, (int) tail.Lines)
@@ -95,11 +104,11 @@ namespace BaseAsyncRunner
                 }
 
                 if (builder.Length > 0) Console.WriteLine(builder.ToString());
-                
+
                 //int i = 0;
                 //int j = 100 / i;
 
-                var cut = new Customer { FirstName = "Jens", LastName = "Nielsen" };
+                var cut = new Customer {FirstName = "John", LastName = "Smith"};
                 var dto = this.mapper.Map<CustomerDto>(cut);
                 Console.WriteLine("Mapped to dto: " + dto.FullName);
             }
@@ -108,6 +117,11 @@ namespace BaseAsyncRunner
                 this.log.Write("Oops " + ex.Message);
                 return -2;
             }
+            finally
+            {
+                // When the task is ready release the semaphore, or it will lock forever.
+                mutex.Release();
+            }
 
             return result;
         }
@@ -115,22 +129,13 @@ namespace BaseAsyncRunner
         private static string ReadLines(string fileName, bool fromTop, int count)
         {
             var lines = File.ReadAllLines(fileName);
-            if (fromTop)
-            {
-                return string.Join(Environment.NewLine, lines.Take(count));
-            }
-            return string.Join(Environment.NewLine, lines.Reverse().Take(count));
+            return string.Join(Environment.NewLine, fromTop ? lines.Take(count) : lines.Reverse().Take(count));
         }
 
         private static string ReadBytes(string fileName, bool fromTop, int count)
         {
             var bytes = File.ReadAllBytes(fileName);
-            if (fromTop)
-            {
-                return Encoding.UTF8.GetString(bytes, 0, count);
-            }
-            return Encoding.UTF8.GetString(bytes, bytes.Length - count, count);
+            return fromTop ? Encoding.UTF8.GetString(bytes, 0, count) : Encoding.UTF8.GetString(bytes, bytes.Length - count, count);
         }
     }
-
 }
